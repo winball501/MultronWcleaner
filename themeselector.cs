c4208a55c14c61fae8b0e26c50f4fad563a8f191
winball501 <winball501@gmail.com>
2025-01-13 17:09:57 -0800

    bug fixes

---
+++ themeselector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MultronWinCleaner
+{
+    class themeselector
+    {
+        public static void selector(Uri uri)
+        {
+
+            ResourceDictionary theme = new ResourceDictionary()
+            {
+                Source = uri,
+            };
+
+            App.Current.Resources.Clear();
+            App.Current.Resources.MergedDictionaries.Add(theme);
+         
+
+        }
+    }
+}
