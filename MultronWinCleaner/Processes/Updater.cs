0f26a32ba2660497e74f326b2899435a7fc8ca2f
winball501 <winball501@gmail.com>
2025-08-22 21:13:29 +0300

    Update Updater.cs
    Version Updated

--- MultronWinCleaner/Processes/Updater.cs
+++ MultronWinCleaner/Processes/Updater.cs
@@ -163,7 +163,7 @@ namespace MultronWinCleaner.Processes
                 }
 
                 Version latestVersion = new Version(match.Value);
-                Version currentVersion = new Version("1.20.3");
+                Version currentVersion = new Version("1.20.4");
 
                 if (latestVersion == currentVersion)
                 {
