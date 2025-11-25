43451cd5621be4a70cbda8c03e9d6b9b0e88bb23
winball501 <winball501@gmail.com>
2025-11-25 23:12:52 +0300

    Update current version to 1.20.8

--- MultronWinCleaner/Processes/Updater.cs
+++ MultronWinCleaner/Processes/Updater.cs
@@ -163,7 +163,7 @@ namespace MultronWinCleaner.Processes
                 }
 
                 Version latestVersion = new Version(match.Value);
-                Version currentVersion = new Version("1.20.7");
+                Version currentVersion = new Version("1.20.8");
 
                 if (latestVersion == currentVersion)
                 {
