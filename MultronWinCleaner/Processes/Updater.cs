5e8c707a2be82d67f8c370d02fca88b3a7d32438
winball501 <winball501@gmail.com>
2026-09-12 14:51:43 +0300

    v1.21.1
    Changed updater version

--- MultronWinCleaner/Processes/Updater.cs
+++ MultronWinCleaner/Processes/Updater.cs
@@ -163,7 +163,7 @@ namespace MultronWinCleaner.Processes
                 }
 
                 Version latestVersion = new Version(match.Value);
-                Version currentVersion = new Version("1.23");
+                Version currentVersion = new Version("1.23.1");
 
                 if (latestVersion == currentVersion)
                 {
--- MultronWinCleaner/obj/Debug/net8.0-windows/MemCleaner.g.i.cs
+++ MultronWinCleaner/obj/Debug/net8.0-windows/MemCleaner.g.i.cs
@@ -157,7 +157,7 @@ namespace MultronWinCleaner {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/MultronWinCleaner;V1.0.0.0;component/memcleaner.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MultronWinCleaner;component/memcleaner.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MemCleaner.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
